2a193e51efff5d9273a3e66edd1d0f6e0d771a7a
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2024-07-13 01:54:41 -0700

    progress on API fetch functions

--- vite-project/src/assets/initialData.js
+++ vite-project/src/assets/initialData.js
@@ -1,4 +1,30 @@
-// I need an array of 16 unique things
+// I need an array of 16 unique Pokemon, both names and sprites
+
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+async function fetchPokemonData() {
+  const response = await fetch(
+    "https://pokeapi.co/api/v2/pokemon-species?limit=1",
+    {
+      mode: "cors",
+    },
+  );
+
+  const pokeData = await response.json();
+  const pokeCount = pokeData.count;
+
+  let rando = getRandomIntInclusive(1, pokeCount);
+
+  // next, assemble into array of 9 (3x3 grid), no repeats
+
+  console.log(rando);
+}
+
+fetchPokemonData();
 
 const initialData = {
   allData: {
